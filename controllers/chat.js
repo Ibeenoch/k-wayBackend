21d6aa38f71d360b53449494848fdfd51b27da7e
Ibewunjo Enoch <fredenoch1@gmail.com>
2024-07-09 15:32:39 +0100

    added stories numbers of views

--- controllers/chat.js
+++ controllers/chat.js
@@ -6,7 +6,7 @@ try {
     console.log(req.body)
   const chatId = await ChatId.find();
 
-  if(chatId.length < 1){
+  if(chatId.length > 0){
       const newChatId = await ChatId.create({
         userId: req.body.userId,
         myId: req.body.myId,
